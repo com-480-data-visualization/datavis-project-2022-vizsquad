0ba94f9ae3262195b44885af0e8a1e79add3600b
RubenB1 <RubenB1@github.com>
2022-06-03 16:24:34 +0200

    cleaning and preparing for final submission

--- deployment repositories/data-viz-course-frontend-main/src/utils/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const HTTP = axios.create({
-    baseURL: `https://data-viz-course-backend-e4sglwkopa-uc.a.run.app/`,
-    headers: {
-        internal_auth_token: 'data_viz',
-    }
-})
